185d632df6bea50629e97e1d276122d6eb41dd83
Davedigital <agbooladavid001@outlook.com>
2025-02-10 18:10:34 +0000

    Updated the package.json file and used "npm install" to set up necessary build commands

--- server.mjs
+++ server.mjs
@@ -16,7 +16,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors({
